193f5b186648233ee60ab4c60f48b5e4b9654c8f
patricio <parango@tvemart.com>
2014-06-19 15:12:29 -0300

    two

---
+++ www/js/scripts.js
@@ -0,0 +1,40 @@
+﻿document.addEventListener("deviceready", function()  {
+	var maximo = 16;
+	var number = 1 + Math.floor(Math.random() * maximo);
+	var claserandom = "bg" + number;
+	var claseprecarga = "bg" + (number + 1);
+	$("#contenedor1").removeClass("bg1");
+	$("#contenedor1").addClass(claserandom);
+	$("#precargador").removeClass("bg2");
+	$("#precargador").addClass(claseprecarga);
+
+        $.post("http://autoplay.es/racing_calendar/m/eventos.php", { ready: "1"}, function( data ) {
+			var eventos = $('#eventos').empty();
+			for (var i=0; i<data.length; i++) {
+					if (data[i].destacado == "1") {
+						
+						 eventos.append('<h2 class="roboto"><span class="bloque2">' + data[i].fecha + " - " + data[i].categoria + ": " + data[i].carrera + '</span></h2>');
+					}
+					else {
+						eventos.append('<h2 class="roboto"><span class="bloque2">' + data[i].fecha + " - " + data[i].categoria + ": " + data[i].carrera + '</span></h2>');
+					}
+			}	 
+		}, "json");
+    }, false);
+
+$( "#reload23" ).click(function(e) {
+	e.preventDefault();
+	var maximo2 = 16;
+	$("#contenedor1").removeClass($('#contenedor1').attr('class'));
+	$("#contenedor1").addClass($('#precargador').attr('class')); 
+	var clase_nueva = $('#precargador').attr('class');
+	$("#precargador").removeClass(clase_nueva);
+	var numero_clase = clase_nueva.substring(2);
+	numero_clase = Number(numero_clase);
+	if (numero_clase == maximo2) {
+		$("#precargador").addClass("bg1"); 
+	}
+	else {
+		$("#precargador").addClass("bg" + (numero_clase + 1)); 	
+	}
+});
---
+++ www/m/eventos.php
@@ -0,0 +1,44 @@
+﻿<?
+$dbHost = "awdb2013.db.8921995.hostedresource.com";
+$dbUser = "awdb2013";
+$dbPass = "GolPower!2011";
+$dbName = "awdb2013";
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass , $dbName);
+if ($conn->connect_errno) {
+    echo "Falló la conexión a MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
+}
+
+$diadehoy = date("l");
+if ($diadehoy === "Monday") {
+	$diainicio = date("Ymd");
+}
+else {
+	$diainicio = date("Ymd", strtotime("Last Monday"));
+}
+if ($diadehoy === "Sunday") {
+	$diafin = date("Ymd", strtotime("Next Sunday"));
+}
+else {
+$diafin = date("Ymd", strtotime("This Sunday"));
+}
+
+
+$sql="SELECT c.carrera, DATE_FORMAT(c.fecha,'%d/%m') AS fecha2, c.destacado, ca.shortname AS categoria
+FROM carreras c
+INNER JOIN subcategorias s ON c.id_subcategoria=s.id
+INNER JOIN categorias ca ON s.id_categoria=ca.id
+WHERE c.fecha BETWEEN ".$diainicio." AND ".$diafin." AND mostrar = 1
+ORDER BY c.fecha";
+$res = $conn->query($sql);
+while ($row = $res->fetch_array()) {
+	$result[] = array(
+			'carrera' => $row["carrera"],
+			'categoria' => $row["categoria"],
+			'fecha' => $row["fecha2"],
+			'destacado' => $row["destacado"],
+		);
+}
+echo json_encode($result);
+
+?>
