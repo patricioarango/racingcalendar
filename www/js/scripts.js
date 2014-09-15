5400041a4c706afcbbd1f5630a0ad68c22f092c1
patricioarango <patricioarango@gmail.com>
2014-09-15 11:11:31 -0300

    two
    two

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -10,24 +10,6 @@ function cortar_fecha(fecha){
         return fecha;
     }
 }    
-    //chequear conexion
-    function checkConnection() {
-    var networkState = navigator.network.connection.type;
-
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Conexión desconocida';
-    states[Connection.ETHERNET] = 'Conexión ethernet';
-    states[Connection.WIFI]     = 'Conexión WiFi';
-    states[Connection.CELL_2G]  = 'Conexión movil 2G';
-    states[Connection.CELL_3G]  = 'Conexión movil 3G';
-    states[Connection.CELL_4G]  = 'Conexión movil 4G';
-    states[Connection.NONE]     = 'Sin conexión';
-
-        if (states[networkState] ==  'Sin conexión') {
-            console.log("sin conexion a internet");
-            window.location.href="no_internet.html?location=index";
-        }
-    }
     // Wait for Cordova to load
     document.addEventListener("deviceready", onDeviceReady, false);
 
