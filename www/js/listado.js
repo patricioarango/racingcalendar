24989dedb530ce1b007854f8f62bfc66c43669f0
patricio <parango@tvemart.com>
2014-09-18 16:19:58 -0300

    last

--- www/js/listado.js
+++ www/js/listado.js
@@ -53,14 +53,14 @@ function cortar_fecha(fecha){
                 var fuente_info = ($( window ).width() * 14 ) / 480;
                 var fuente_distancia = ($( window ).width() * 13 ) / 480;
             //contenido header
-                 header.append('<img style="float: left;" class="anterior_se" src="images/prev2_w.png" height="40px" /><img style="float: right;" class="siguiente_se" src="images/next2_w.png" height="40px" /><div class="texto23"><span class="light" style="font-size:' + fuente_header + 'px;">' + data[0].categoria + '</span></div>' );
+                 header.append('<img style="float: left;margin-left:20px;" class="anterior_se" src="images/back_w.png" height="40px" /><!-- <img style="float: right; margin-right:20px;" class="siguiente_se" src="images/next2_w.png" height="40px" />--><div class="texto23"><span class="ultra-bold" style="font-size:' + fuente_header + 'px;">category/</span><span class="light" style="font-size:' + fuente_header + 'px;">' + data[0].categoria + '</span></div>' );
             for (var i=0; i<data.length; i++) {
                 //buscamos la distancia siempre que el circuito no sea el "240"
                 if (data[i].circuito_id != "240") {
                     var p1 = LatLon(Geo.parseDMS(latitud), Geo.parseDMS(longitud));
                     var p2 = LatLon(Geo.parseDMS(data[i].latitud), Geo.parseDMS(data[i].longitud));
-                    var distancia = '<img src="images/location.png" height="15px">' + Math.ceil(p1.distanceTo(p2)) + " kms ";
-                    var circuito = '<img src="images/circuit-icon.png" height="15px">' + data[i].circuito;
+                    var distancia = '<img src="images/location_b.png" height="15px">' + Math.ceil(p1.distanceTo(p2)) + " kms ";
+                    var circuito = '<img src="images/circuit-icon_b.png" height="15px">' + data[i].circuito;
                 }
                 else {
                     var distancia = "";
@@ -75,26 +75,19 @@ function cortar_fecha(fecha){
                 }
                 //con nro fecha
                 if (data[i].nro_fecha > 0 ){
-                    var nro_fecha = " Round" + data[i].nro_fecha;
+                    var nro_fecha = " <span class='ultra-bold'>ROUND</span>" + data[i].nro_fecha;
                 }
                 else {
                     var nro_fecha = "";
-                }
-                //nombre corto para categoria si el nombre es largo
-                if (data[i].categoria.length > 19) {
-                    var categoria = data[i].categoria_short;
-                }
-                else {
-                    var categoria = data[i].categoria;
                 }
                 var dia = data[i].fecha.substr(0,2);
                 var mes = data[i].fecha.substr(3,2);
                 
                 //altura para cada div
-                var altura_listado = ($( window ).width() * 600 ) / 1600;
+                var altura_listado = ($( window ).width() * 300 ) / 1600;
                 
                 //contenido body
-                eventos.append('<div class="listado" style="height:'+ altura_listado + 'px; ">'+ destacado + '<div class="principal" style="font-size:' + fuente_principal + 'px;"><span class="ultra-bold">' + categoria + ': </span><span class="light">' + data[i].carrera + '</span></div><div class="info" style="font-size:' + fuente_info + 'px;"><span class="ultra-bold">' + monthNames[cortar_fecha(mes)] + '</span><span class="light">' + cortar_fecha(dia) + nro_fecha + '</span></div><div class="distancia" style="font-size:' + fuente_distancia + 'px;"> ' + distancia + '</div><div class="distancia"> ' + circuito + '</div></div>');
+                eventos.append('<div class="listado" style="height:'+ altura_listado + 'px; "><div class="listado_izq" style="height:'+ altura_listado + 'px; "><span class="light" style="font-size:50px;">' + cortar_fecha(dia) + '</span><br><span class="light" style="font-size:20px;">' + monthNames[cortar_fecha(mes)] + '</span></div><div class="listado_der" style="height:'+ altura_listado + 'px; ">'+ destacado + '<div class="principal" style="font-size:' + fuente_principal + 'px;"><span class="ultra-bold">' + data[i].carrera + '</span></div><div class="info" style="font-size:' + fuente_info + 'px;"><span class="ultra-bold">' + monthNames[cortar_fecha(mes)] + '</span><span class="light">' + cortar_fecha(dia) + nro_fecha + '</span></div><div class="distancia" style="font-size:' + fuente_distancia + 'px;"> ' + distancia + '</div><div class="distancia distancia_final"> ' + circuito + '</div></div></div>');
             } //cierra for
         }, "json");
        
@@ -115,14 +108,7 @@ function getUrlParameter(sParam)
 }  
 $("header").on('click',".anterior_se",function(e) {
     e.preventDefault();
-   var posicion = getUrlParameter('posicion');
-   if (posicion == null) {
-        posicion = "-1";
-   }
-   else {
-        posicion = +posicion - 1;  
-   }
-   window.location.href='index.html?posicion=' + posicion;
+   window.location.href='index.html';
 });
 $("header").on('click',".siguiente_se",function(e) {
     e.preventDefault();
